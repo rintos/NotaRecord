f9774329b943101a647d8c42821778ba7b180cb6
VictorSantoryu <victor.santoryu@github.com>
2018-12-20 15:22:07 -0200

    adcionado alerta

---
+++ NotaRecord/ListaNotaAudioTableViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ListaNotaAudioTableViewController.swift
+//  NotaRecord
+//
+//  Created by Victor on 20/12/2018.
+//  Copyright © 2018 Rinver. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ListaNotaAudioTableViewController: UITableViewController {
+
+    var context: NSManagedObjectContext!
+    var notaAudio: [NSManagedObject] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate.persistentContainer.viewContext
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        recuperaNotas()
+    }
+
+
+    func recuperaNotas(){
+        
+        let requisicao = NSFetchRequest<NSFetchRequestResult>(entityName: "NotaAudio")
+        
+        do {
+          let notaRecuperada = try context.fetch(requisicao)
+          self.notaAudio = notaRecuperada as! [NSManagedObject]
+            tableView.reloadData()
+        } catch let erro {
+            print("erro ao listar notas: \(erro)")
+        }
+        
+    }
+    
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return notaAudio.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celulaNota", for: indexPath)
+
+        let notaAudioRecuperado = self.notaAudio[indexPath.row]
+        let textoRecuperado = notaAudioRecuperado.value(forKey: "texto")
+        let dataRecuperada = notaAudioRecuperado.value(forKey: "data")
+       
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        
+        let novaDataRecuperada = dateFormatter.string(from: dataRecuperada as! Date)
+        
+        cell.textLabel?.text = textoRecuperado as? String
+        cell.detailTextLabel?.text = novaDataRecuperada
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NotaRecord/ViewController.swift
+++ NotaRecord/ViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    var context: NSManagedObjectContext!
+    var notaAudio: NSManagedObject!
+    
+    @IBOutlet weak var textoTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.textoTextField.text = ""
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = appDelegate.persistentContainer.viewContext
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
+    @IBAction func salvar(_ sender: Any?) {
+        salvarNotaAudio()
+        
     }
 
+    
+    func salvarNotaAudio(){
+        
+        let novaAudioNota = NSEntityDescription.insertNewObject(forEntityName:"NotaAudio" , into: context)
+        
+        novaAudioNota.setValue(self.textoTextField.text, forKey: "texto" )
+        novaAudioNota.setValue( Date(), forKey: "data")
+        
+        do {
+            try context.save()
+            print("Nota Salva Com Sucesso")
+            
+            let alerta = UIAlertController(title: "Alerta", message: "Salvo com Sucesso", preferredStyle: .actionSheet)
+            let ok = UIAlertAction(title: "ok", style: .default, handler: nil)
+            
+            alerta.addAction(ok)
+                
+            present(alerta, animated: true, completion: nil)
+            
+            self.textoTextField.text = ""
+            
+        } catch let erro {
+            print("Nao foi possivel salvar nota erro: \(erro)")
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
