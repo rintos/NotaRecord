9a0ff81070f1639421ae427b3dc0d63879b75552
VictorSantoryu <victor.santoryu@github.com>
2018-12-27 23:06:14 -0200

    Implementado AVFoundation Gravar e dar play em audio

--- NotaRecord/ViewController.swift
+++ NotaRecord/ViewController.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 import CoreData
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioRecorderDelegate,AVAudioPlayerDelegate {
     
     var context: NSManagedObjectContext!
     var notaAudio: NSManagedObject!
     
-    @IBOutlet weak var textoTextField: UITextField!
     
+    @IBOutlet weak var textoTextField: UITextField!
+
+    @IBOutlet weak var botaoGravar: UIButton!
+    
+    @IBOutlet weak var botaoPlay: UIButton!
+    
+    //variavel do tipo Audio
+    var soundRecorder: AVAudioRecorder!
+    var soundPlayer: AVAudioPlayer!
+    
+    var fileName: String = "meuAudioFile.m4a"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +35,86 @@ class ViewController: UIViewController {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         context = appDelegate.persistentContainer.viewContext
 
+        
+        setupRecorder()
+        botaoPlay.isEnabled = false
     }
-
+    
+    //recupero/seto o caminho do arquivo de audio
+    func getDocumentDirector() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    //cria o arquivo adicionando caminho // grava suas propriedades
+    func setupRecorder(){
+        let audioFilename = getDocumentDirector().appendingPathComponent(fileName)
+        let recordSetting = [ AVFormatIDKey : kAudioFormatAppleLossless,
+                              AVEncoderAudioQualityKey : AVAudioQuality.max.rawValue,
+                              AVEncoderBitRateKey : 320000,
+                              AVNumberOfChannelsKey : 2,
+                              AVSampleRateKey : 44100.2 ] as [String : Any]
+        do {
+            soundRecorder = try AVAudioRecorder(url: audioFilename, settings: recordSetting)
+            soundRecorder.delegate = self
+            soundRecorder.prepareToRecord()
+            
+        } catch  {
+            print(error)
+        }
+        
+    }
+    
+    //roda o audio
+    func setupPlayer(){
+        let audioFilename = getDocumentDirector().appendingPathComponent(fileName)
+        do {
+           soundPlayer = try AVAudioPlayer(contentsOf: audioFilename)
+            soundPlayer.delegate = self
+            soundPlayer.prepareToPlay()
+            soundPlayer.volume = 3.0
+        } catch  {
+            print(error)
+        }
+    }
+    
+    //quando audio terminar de gravar/ alerta/
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        botaoPlay.isEnabled = true
+    }
+    
+    //quando o audio terminar de rodar/ alerta/
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        botaoGravar.isEnabled = true
+        botaoPlay.setTitle("Play", for: .normal)
+    }
+    
+    //o gatilho dessa funcao ativa
+    @IBAction func gravarAction(_ sender: Any){
+        if botaoGravar.titleLabel?.text == "Gravar" {
+            soundRecorder.record()
+            botaoGravar.setTitle("Parar", for: .normal)
+            botaoPlay.isEnabled = false
+        }else {
+            soundRecorder.stop()
+            botaoGravar.setTitle("Gravar", for: .normal)
+            botaoPlay.isEnabled = false
+        }
+    }
+    
+    @IBAction func playAction(_ sender: Any) {
+        if botaoPlay.titleLabel?.text == "Play" {
+            botaoPlay.setTitle("Parar", for: .normal)
+            botaoGravar.isEnabled = false
+            setupPlayer()
+            soundPlayer.play()
+        }else {
+            soundPlayer.stop()
+            botaoPlay.setTitle("Play", for: .normal)
+            botaoGravar.isEnabled = false
+        }
+    }
+    
  
     @IBAction func salvar(_ sender: Any?) {
         salvarNotaAudio()
